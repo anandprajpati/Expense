77242fbe27db5f6d91d7ad9dbe37c90ac4e1a8f4
anandprajpati <116326744+anandprajpati@users.noreply.github.com>
2025-11-21 23:24:04 +0530

    Update url.js

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -1,3 +1,4 @@
 // export const  BASE_URL =  "http://localhost:8080/api/v1/"
 // utils/url.js
-export const BASE_URL = "http://localhost:8080/api/v1"; // ✅ no trailing slash
+export const BASE_URL = "https://expense-backend-78d0.onrender.com/api/v1";
+// ✅ no trailing slash
